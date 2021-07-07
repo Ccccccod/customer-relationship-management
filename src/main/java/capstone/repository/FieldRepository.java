a6727d3a8e732559b7a4244cad3f3923868c99ba
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-07-08 00:28:19 +0700

    Removed unnecessary imports

--- src/main/java/capstone/repository/FieldRepository.java
+++ src/main/java/capstone/repository/FieldRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import capstone.entity.Field;
-import capstone.entity.Source;
 
 /**
  * Repository for {@link Field}

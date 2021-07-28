7d03cc1e541dd10ce00da8954f4a97bc1a4d28b9
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-07-29 02:31:30 +0700

    Added lombok.Builder

--- src/main/java/capstone/entity/Source.java
+++ src/main/java/capstone/entity/Source.java
@@ -3,6 +3,7 @@
  */
 package capstone.entity;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -55,6 +57,26 @@ public class Source extends NamedEntity<Long> {
 	@EqualsAndHashCode.Exclude
 	@JsonIgnore
 	private Set<Opportunity> opportunities;
+
+	/**
+	 * @param id
+	 * @param createdAt
+	 * @param updatedAt
+	 * @param createdBy
+	 * @param updatedBy
+	 * @param name
+	 * @param customers
+	 * @param contacts
+	 * @param opportunities
+	 */
+	@Builder
+	Source(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, User createdBy, User updatedBy, String name,
+			Set<Customer> customers, Set<Contact> contacts, Set<Opportunity> opportunities) {
+		super(id, createdAt, updatedAt, createdBy, updatedBy, name);
+		this.customers = customers;
+		this.contacts = contacts;
+		this.opportunities = opportunities;
+	}
 
 	public Source(String name) {
 		super(name);

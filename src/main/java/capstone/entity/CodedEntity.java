e5607f95495c64ce2de3251249ea4d7c4843fe55
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-08-10 23:53:06 +0700

    Moved all args constructor to before no args constructor

--- src/main/java/capstone/entity/CodedEntity.java
+++ src/main/java/capstone/entity/CodedEntity.java
@@ -32,6 +32,55 @@ import lombok.ToString;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@MappedSuperclass
+public abstract class CodedEntity<ID extends Serializable> extends BaseEntity<ID> {
+	private static final long serialVersionUID = 1L;
+
+	@GenericGenerator( //
+			name = "assigned-sequence", //
+			strategy = "capstone.utils.StringSequenceIdentifierGenerator", //
+			parameters = { //
+					@org.hibernate.annotations.Parameter( //
+							name = "sequence_name", value = "hibernate_sequence"), //
+//                @org.hibernate.annotations.Parameter(
+//                        name = "sequence_prefix", value = "BE"),
+			})
+	@GeneratedValue(generator = "assigned-sequence", strategy = GenerationType.SEQUENCE)
+	@Column(name = "code", unique = true, nullable = false)
+	private String code;
+
+	/**
+	 * Sequence at the start of code EX: KH00001
+	 * @return
+	 */
+	public abstract String sequencePrefix();
+
+	/**
+	 * Length of number in code EX: KH00001 -> length = 5
+	 * @return length of number in code
+	 */
+	public abstract int numberIdLength();
+
+	/**
+	 * @param id
+	 * @param createdAt
+	 * @param updatedAt
+	 * @param createdBy
+	 * @param updatedBy
+	 */
+	public CodedEntity(ID id, LocalDateTime createdAt, LocalDateTime updatedAt, User createdBy, User updatedBy) {
+		super(id, createdAt, updatedAt, createdBy, updatedBy);
+	}
+
+	/**
+	 * @author Tuna
+	 *
+	 */
+	@EqualsAndHashCode
+	@ToString
+	@Getter
+	@Setter
+	@NoArgsConstructor
 	@AllArgsConstructor
 
 //	@Entity

84cca40be4a993df7c8a312287c2de72b07ef7e0
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-08-12 09:03:08 +0700

    Added no args constructor

--- src/main/java/capstone/exception/ResourceNotFoundException.java
+++ src/main/java/capstone/exception/ResourceNotFoundException.java
@@ -24,6 +24,9 @@ public class ResourceNotFoundException extends Exception {
 	public ResourceNotFoundException(String message) {
 		super(message);
 	}
-	
+
+	public ResourceNotFoundException() {
+		super();
+	}
 
 }

f54ffe3a46373de6d37dee6b8a86fab1a140f88c
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-08-15 21:23:15 +0700

    Use Enum for OpportunityPhase instead of Class

---
+++ src/main/java/capstone/common/enums/OpportunityPhase.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package capstone.common.enums;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import capstone.dto.response.serializer.I18nEnumSerializer;
+import capstone.i18n.I18nEnum;
+
+/**
+ * OpportunityPhase
+ * Giai đoạn cơ hội
+ * @author Tuna
+ *
+ */
+@JsonSerialize(using = I18nEnumSerializer.class)
+public enum OpportunityPhase implements I18nEnum {
+
+	/** Mở đầu */
+	BEGINNING,
+	
+	/** Khách hàng tự tìm kiếm */
+	CUSTOMER_INTEREST,
+
+	/** DEMO / giới thiệu */
+	DEMO,
+
+	/** Đàm phán, thương lượng */
+	NEGOTIATION,
+
+	/** Kết thúc thành công */
+	SUCCESS_FINISH,
+
+	/** Kết thúc thất bại */
+	FAILURE_FINISH
+}

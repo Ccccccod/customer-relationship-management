978132d25f3e37e26897ae3960c50bbd623dc51d
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-08-18 11:02:26 +0700

    Added Gender enum

---
+++ src/main/java/capstone/common/enums/Gender.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package capstone.common.enums;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import capstone.dto.response.serializer.I18nEnumSerializer;
+import capstone.i18n.I18nEnum;
+
+/**
+ * Gender
+ * Giới tính
+ * @author Tuna
+ *
+ */
+@JsonSerialize(using = I18nEnumSerializer.class)
+public enum Gender implements I18nEnum {
+	
+	/** Nam */
+	MALE,
+
+	/** Nữ */
+	FEMALE,
+
+	/** Khác */
+	OTHER
+
+}

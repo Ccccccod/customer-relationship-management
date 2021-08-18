1d21934a3c09413dcd6a3816ad1523ee1d816c8e
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-08-18 11:25:39 +0700

    Added MaritalStatus enum

---
+++ src/main/java/capstone/common/enums/MaritalStatus.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package capstone.common.enums;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import capstone.dto.response.serializer.I18nEnumSerializer;
+import capstone.i18n.I18nEnum;
+
+/**
+ * MaritalStatus
+ * Tình trạng hôn nhân
+ * @author Tuna
+ *
+ */
+@JsonSerialize(using = I18nEnumSerializer.class)
+public enum MaritalStatus implements I18nEnum {
+	
+	/** Chưa kết hôn */
+	NOT_MARRIED,
+
+	/** Đã kết hôn */
+	MARRIED,
+
+	/** Đã ly hôn */
+	DIVORCED,
+
+	/** Khác */
+	OTHER
+
+}

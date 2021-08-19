61d97505c5b4eb0b6af0b0040c73f9e61e91f794
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-08-20 02:37:26 +0700

    Fixed update profile not working

--- src/main/java/capstone/controller/ProfileController.java
+++ src/main/java/capstone/controller/ProfileController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import capstone.entity.Role;
 import capstone.entity.User;
 import capstone.exception.ResourceNotFoundException;
 import capstone.repository.RoleRepository;
+import capstone.repository.UserRepository;
 import capstone.service.AbstractService;
 import capstone.service.UserService;
 
@@ -36,9 +38,22 @@ public class ProfileController {
 
 	@Autowired
 	private RoleRepository roleRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Autowired
 	private UserService userService;
+	
+	@GetMapping
+	public ResponseEntity<UserResponse> getProfile() throws ResourceNotFoundException {
+		User currentUser = userService.getCurrentUser();
+		if (Objects.isNull(currentUser))
+			throw new ResourceNotFoundException("Can not find current user!");
+
+		UserResponse response = entityToResponse(currentUser);
+		return ResponseEntity.ok(response);
+	}
 	
 	@PutMapping
 	public ResponseEntity<UserResponse> updateProfile(@RequestBody UserUpdateDto dto) throws ResourceNotFoundException {
@@ -50,6 +65,7 @@ public class ProfileController {
 		logger.debug("update() of id#{} with body {}", id, dto);
 		
 		currentUser = updateEntity(dto, currentUser);
+		userRepository.save(currentUser);
 		logger.debug("updated enitity: {}", currentUser);
 		
 		UserResponse response = entityToResponse(currentUser);
--- src/main/java/capstone/controller/UserController.java
+++ src/main/java/capstone/controller/UserController.java
@@ -37,7 +37,7 @@ public class UserController extends AbstractCRUDController<UserDto, UserUpdateDt
 	protected UserResponse entityToResponse(User user) {
 		return UserResponse.builder()
 				.id(user.getId())
-				.username(user.getName())
+				.username(user.getUsername())
 				.email(user.getEmail())
 				.roles(user.getRoles())
 				.createdAt(user.getCreatedAt())

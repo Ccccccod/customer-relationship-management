78bac8ffbe58edce8999b704f72b2d11206104fc
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-08-25 16:12:02 +0700

    Removed validation

--- src/main/java/capstone/dto/request/LoginRequest.java
+++ src/main/java/capstone/dto/request/LoginRequest.java
@@ -5,8 +5,6 @@ package capstone.dto.request;
 
 import javax.validation.constraints.NotNull;
 
-import capstone.dto.validatation.annotation.Password;
-import capstone.dto.validatation.annotation.Username;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,10 +22,8 @@ import lombok.ToString;
 public class LoginRequest {
 
 	@NotNull
-	@Username
 	private String username;
 
 	@NotNull
-	@Password
 	private String password;
 }

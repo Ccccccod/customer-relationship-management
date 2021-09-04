5868b2a9e0fa81ad7aae592c20f4cc83a167514e
Tran Minh Kien <tranminhkientc@gmail.com>
2021-09-04 20:54:08 +0700

    update order

--- src/main/java/capstone/service/OrderService.java
+++ src/main/java/capstone/service/OrderService.java
@@ -9,7 +9,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import capstone.dto.request.OrderDto;
 import capstone.entity.Order;
+import capstone.exception.ResourceNotFoundException;
 import capstone.repository.OrderRepository;
 
 /**
@@ -18,13 +20,48 @@ import capstone.repository.OrderRepository;
  *
  */
 @Service
-public class OrderService {
+public class OrderService extends AbstractService< OrderDto,  OrderDto,  Order,  Order,  OrderRepository, Long>{
 	
 	@Autowired
 	private OrderRepository repository;
 	
 	public List<Order> findByOrderDateBetween(LocalDate from, LocalDate to) {
 		return repository.findByOrderDateBetween(from, to);
+	}
+
+	@Override
+	protected Class<Order> entityClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Order entityToResponse(Order entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Order createDtoToEntity(OrderDto d, Order entity) throws ResourceNotFoundException {
+		return entity.toBuilder()
+				.id(d.getId())
+				.code(d.getCode())
+				.orderDate(d.getOrderDate())
+				.customer(customerService.getEntityById(d.getCustomerId()))
+				.contact(contactService.getEntityById(d.getContactId()))
+				.opportunity(opportunityService.getEntityById(d.getOpportunityId()))
+				.explanation(d.getExplanation())
+				.liquidationDeadline(d.getLiquidationDeadline())
+				.deliveryDeadline(d.getDeliveryDeadline())
+				.receivedMoney(d.getReceivedMoney())
+				.build();
+
+	}
+
+	@Override
+	protected Order updateDtoToEntity(OrderDto updateDto, Order entity) throws ResourceNotFoundException {
+		return this.createDtoToEntity(updateDto, entity);
+
 	}
 
 }

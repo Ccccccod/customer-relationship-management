cec0cab5def3c0f2c8dbcaa553a5a15576bf1e33
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-09-05 22:24:34 +0700

    Fixed swagger bug

--- src/main/java/capstone/config/WebSecurityConfig.java
+++ src/main/java/capstone/config/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -74,6 +75,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources/**",
+                                   "/configuration/security",
+                                   "/swagger-ui.html",
+                                   "/swagger-ui/**",
+                                   "/webjars/**");
+    }
+
 	/**
 	 * Swagger ui
 	 */
@@ -125,7 +137,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		}
         
 		http.authorizeRequests()
-//        .anyRequest().authenticated()
+        .anyRequest().authenticated()
         .and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		

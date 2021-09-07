21d31304ff0ccecd1c3ba7702137400a124dc6c9
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-09-07 10:02:10 +0700

    Formatted

--- src/main/java/capstone/service/InvoiceService.java
+++ src/main/java/capstone/service/InvoiceService.java
@@ -16,7 +16,7 @@ import capstone.repository.InvoiceRepository;
  * @author tuna
  */
 @Service
-public class InvoiceService extends AbstractService<InvoiceDto , InvoiceDto, Invoice, Invoice,InvoiceRepository, Long> {
+public class InvoiceService extends AbstractService<InvoiceDto, InvoiceDto, Invoice, Invoice, InvoiceRepository, Long> {
 
 	@Override
 	protected Class<Invoice> entityClass() {
@@ -29,28 +29,25 @@ public class InvoiceService extends AbstractService<InvoiceDto , InvoiceDto, Inv
 	}
 
 	@Override
-	protected Invoice createDtoToEntity(InvoiceDto dto , Invoice entity) throws ResourceNotFoundException {
-		 return  entity.toBuilder()
-		.code(dto.getCode())
-		.customer(customerService.getEntityById(dto.getCustomerId()))
-		.address(dto.getAddress())
-		.bankAccount(dto.getBankAccount())
-		.bank(dto.getBank())
-		.taxCode(dto.getTaxCode())
-		.buyer(contactService.getEntityById(dto.getBuyerId()))
-		.receiverName(dto.getReceiverName())
-		.receiverEmail(dto.getReceiverEmail())
-		.receiverPhone(dto.getReceiverPhone())
-		.order(orderService.getEntityById(dto.getOrderId()))
-		.build();
+	protected Invoice createDtoToEntity(InvoiceDto dto, Invoice entity) throws ResourceNotFoundException {
+		return entity.toBuilder() //
+				.code(dto.getCode()) //
+				.customer(customerService.getEntityById(dto.getCustomerId())) //
+				.address(dto.getAddress()) //
+				.bankAccount(dto.getBankAccount()) //
+				.bank(dto.getBank()) //
+				.taxCode(dto.getTaxCode()) //
+				.buyer(contactService.getEntityById(dto.getBuyerId())) //
+				.receiverName(dto.getReceiverName()) //
+				.receiverEmail(dto.getReceiverEmail()) //
+				.receiverPhone(dto.getReceiverPhone()) //
+				.order(orderService.getEntityById(dto.getOrderId())) //
+				.build();
 	}
 
 	@Override
 	protected Invoice updateDtoToEntity(InvoiceDto updateDto, Invoice entity) throws ResourceNotFoundException {
 		return this.createDtoToEntity(updateDto, entity);
 	}
-
-	
-	
 
 }

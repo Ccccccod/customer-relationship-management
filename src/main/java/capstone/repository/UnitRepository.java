73b07d33db62de1e03570c7757924da7ab3ca3a7
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-09-08 11:37:01 +0700

    Added Unit entity

---
+++ src/main/java/capstone/repository/UnitRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package capstone.repository;
+
+import org.springframework.stereotype.Repository;
+
+import capstone.entity.Unit;
+
+/**
+ * UnitRepository
+ * @author Tuna
+ */
+@Repository
+public interface UnitRepository extends NamedJpaRepository<Unit, Long> {
+
+}

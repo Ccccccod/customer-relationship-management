4989768a2603215351b26462e91745f688b68c31
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-09-10 20:40:48 +0700

    Return contact

--- src/main/java/capstone/controller/PotentialController.java
+++ src/main/java/capstone/controller/PotentialController.java
@@ -3,6 +3,7 @@
  */
 package capstone.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,10 +29,11 @@ public class PotentialController extends
 		CRUDController<PotentialDto, PotentialDto, Potential, Potential, PotentialRepository, PotentialService, Long> {
 	
 	@PostMapping("/single-convert/{id}")
-	public void singleConvert(@PathVariable(name = "id", required = true) Long id,
+	public ResponseEntity<Contact> singleConvert(@PathVariable(name = "id", required = true) Long id,
 			@RequestParam(name = "customerId") Long customerId)
 			throws ResourceNotFoundException, InstantiationException, IllegalAccessException, ResourceExistedException {
 		Contact contact = service.singleConvert(id, customerId);
+		return ResponseEntity.ok(contact);
 	}
 
 }

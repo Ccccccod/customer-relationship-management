1599a1877a3cd8db53605976a8443e0cb50526c1
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-09-11 08:26:09 +0700

    Accept date with time

--- src/main/java/capstone/dto/request/deserializer/LocalDateDeserializer.java
+++ src/main/java/capstone/dto/request/deserializer/LocalDateDeserializer.java
@@ -5,7 +5,6 @@ package capstone.dto.request.deserializer;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 import org.springframework.boot.json.JsonParseException;
@@ -13,12 +12,12 @@ import org.springframework.boot.json.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 /**
  * LocalDateDeserializer
  * @author Tuna
- *
  */
 public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
 	private static final long serialVersionUID = 1L;
@@ -30,12 +29,32 @@ public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
 	public LocalDateDeserializer() {
 		super(LocalDate.class);
 	}
+	
+//	private static final String[] PATTERNS = new String[] {
+//			"yyyy-MM-dd'T'HH:mm:ss.SSS",
+//			"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+//			"yyyy-MM-dd'T'HH:mm:ss.SSSZ",
+//			"yyyy-MM-dd'T'HH:mm:ss.SSS Z",
+//			"yyyy/MM/dd'T'HH:mm:ss.SSS",
+//			"yyyy-MM-dd",
+//	};
+//	
+//	private static final DateTimeFormatter DATE_TIME_FORMATTER = Arrays.stream(PATTERNS)
+//	        .map(DateTimeFormatter::ofPattern)
+//	        .reduce(new DateTimeFormatterBuilder(), 
+//	                DateTimeFormatterBuilder::appendOptional, 
+//	                (f1, f2) -> f1.append(f2.toFormatter()))
+//	        .toFormatter();
 
 	@Override
-	public LocalDate deserialize(JsonParser p, DeserializationContext ctxt)
+	public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt)
 			throws IOException, JsonProcessingException {
 		try {
-			return LocalDate.parse(p.getText(), DateTimeFormatter.ISO_LOCAL_DATE);
+			JsonNode node = jp.getCodec().readTree(jp);
+			if (!node.isTextual()) return null;
+			if (node.textValue().length() < 10)
+				throw new JsonParseException(new DateTimeParseException(null, node.textValue(), 0));
+			return LocalDate.parse(node.textValue().substring(0, 10));
 		} catch (DateTimeParseException e) {
 			throw new JsonParseException(e);
 		}

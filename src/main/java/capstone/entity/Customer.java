8b017792f41ae20187e8a6f4997ac21b7e49d545
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-09-16 09:55:24 +0700

    Removed unused import

--- src/main/java/capstone/entity/Customer.java
+++ src/main/java/capstone/entity/Customer.java
@@ -16,7 +16,6 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;

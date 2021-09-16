30784a4b2ccff7df092019fa92182a3976fdd38d
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-09-16 19:48:53 +0700

    Added method to get all name with successRate

--- src/main/java/capstone/repository/OpportunityPhaseRepository.java
+++ src/main/java/capstone/repository/OpportunityPhaseRepository.java
@@ -3,9 +3,13 @@
  */
 package capstone.repository;
 
+import java.io.Serializable;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import capstone.entity.OpportunityPhase;
+import capstone.model.IdAndName;
 
 /**
  * OpportunityPhaseRepository
@@ -14,5 +18,15 @@ import capstone.entity.OpportunityPhase;
 @Repository
 public interface OpportunityPhaseRepository
 		extends NamedJpaRepository<OpportunityPhase, Long>, BaseRepository<OpportunityPhase, Long> {
+
+	List<IdAndNameAndSuccessRate<Long>> findIdNameSuccessRateAllBy();
+
+	public interface IdAndNameAndSuccessRate<ID extends Serializable> extends IdAndName<ID> {
+		ID getId();
+
+		String getName();
+
+		Integer getSuccessRate();
+	}
 
 }

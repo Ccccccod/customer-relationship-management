a9b24d408bda8dc920a028af1ab37bfd03203629
Tú Nguyễn <anhtukbhb99@gmail.com>
2021-09-16 21:09:17 +0700

    Updated email regex

--- src/main/java/capstone/common/Constant.java
+++ src/main/java/capstone/common/Constant.java
@@ -15,7 +15,7 @@ public final class Constant {
 		
 		public static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!\\\"#$%&'()*+,-.\\/:;<=>?@\\[\\]^_`{|}~\\\\])(?=\\S+$).{8,40}$";
 		
-		public static final String EMAIL_REGEX = "^[a-z][a-z0-9_\\.]{5,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,2}$";
+		public static final String EMAIL_REGEX = "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
 		
 	}
 	
